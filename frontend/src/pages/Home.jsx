58b4e149bdbfddc8f6b8c1943fbea8e3ca21c2ae
Sathyam Sahu <sathyamrock55@gmail.com>
2024-02-03 18:16:12 +0530

    Paytm done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const token = localStorage.getItem("token");
+    const navigate = useNavigate();
   return (
-    <div>
-        This is the home page. 
+    <div className='flex flex-col items-center justify-center h-screen'>
+        {
+            token ? <div className='text-center py-4 px-6 bg-black text-white rounded-md shadow-sm cursor-pointer' onClick={() => navigate('/dashboard')}>
+                Go to dashboard
+            </div> : <div>
+                <div className='text-center py-4 px-6 bg-black text-white rounded-md shadow-sm cursor-pointer mb-4' onClick={() => navigate('/signup')}>Sign up</div>
+                <div className='text-center py-4 px-6 bg-black text-white rounded-md shadow-sm cursor-pointer mb-4' onClick={() => navigate('/signin')}>Sign in</div>
+            </div>
+        }
     </div>
   )
 }
